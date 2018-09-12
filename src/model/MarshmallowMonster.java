cc6fadf763e83e70ea7cddfb4c83d4dceabeb81a
Bryan Tracy <btracy337@gmail.com>
2018-09-12 08:06:52 -0600

    added setters and getters

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -31,7 +31,54 @@ public class MarshmallowMonster
 	
 		
 	}
-		
+		//Getters
+	public String getName()
+	{
+		//return is automatic exit point of a method.
+		return name;
+	}
+	public double getlegCount()
+	{
+		return legCount;
+	}
+	public boolean gethasNoses()
+	{
+		return hasNoses;
+	}
+	public int getarmCount()
+	{
+		return armCount;
+	}
+	public int geteyeCount()
+	{
+		return eyeCount;
+	}
+	//setters
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setlegCount(double legs)
+	{
+		this.legCount = legs;
+	}
+	
+	public void seteyeCount(int eyes)
+	{
+		this.eyeCount = eyes;
+	}
+	
+	public void sethasNoses(boolean noses)
+	{
+		this.hasNoses = noses;
+	}
+	
+	public void setarmCount(int arms)
+	{
+		this.armCount = arms;
+	}
+	
 	public String toString()
 	{
 		String description = "My monster's name is " + name + " he has " + legCount + " legs" + eyeCount + " eyes" + armCount + " arms";
